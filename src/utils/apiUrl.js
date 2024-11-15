5125724a38234c3b6c730b2da6a838387973d841
hyeonji423 <hyeon423423@naver.com>
2024-11-15 10:51:46 +0900

    test8

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,5 +1,5 @@
 // const rootPath = 'http://localhost:8000'
-const rootPath = 'http://98.80.122.244'
+const rootPath = 'http://54.196.182.57'
 
 const GET_TASKS_API_URL = `${rootPath}/getTasks`
 const DELETE_TASK_API_URL = `${rootPath}/deleteTask`
